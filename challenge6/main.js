8e300f7d9fb98dbb3e7a0a9aa847a4bdd7a8251a
Lorenalgm <lorena@proesc.com>
2020-12-06 22:51:35 -0300

    Added solution to challenge 6

---
+++ challenge6/main.js
@@ -0,0 +1,28 @@
+function sortByLength(strs) {
+    var order = strs.sort(function(a,b){
+        return a.length - b.length
+    });
+    
+    return order;
+}
+
+
+
+/**
+* Test Suite 
+*/
+describe('sortByLength()', () => {
+    it('sorts the strings from shortest to longest', () => {
+        // arrange
+        const strs = ["abc", "", "aaa", "a", "zz"];
+        
+        // act
+        const result = sortByLength(strs);
+
+        // log
+        console.log("result: ", result);
+        
+        // assert
+        expect(result).toEqual(["", "a", "zz", "abc", "aaa"]);
+    });
+});
